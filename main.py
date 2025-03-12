471e8371a5fbcd8e439d90e79104ac2bc813cc26
Dimitar <dimitar.georgiev@primeholding.com>
2025-03-12 23:18:13 +0200

    Add multi monitors support

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import time
 import numpy as np
 import pyautogui
 import ctypes
-from PIL import ImageChops
+import mss
+from PIL import Image, ImageChops
+import os
 
 CHECK_INTERVAL = 2  # The check interval in seconds
-INACTIVITY_THRESHOLD = 30  # The time in seconds of screen inactivity before turning off the monitor
+INACTIVITY_THRESHOLD = 60 * 3  # The time in seconds of screen inactivity before turning off the monitor
+TASKBAR_HEIGHT = 40  # Approximate taskbar height in px in order not to capture the clock change (adjust if needed)
 
 def turn_off_monitor():
     ctypes.windll.user32.PostMessageW(0xFFFF, 0x0112, 0xF170, 2)
@@ -21,7 +24,24 @@ def get_idle_time():
     return (ctypes.windll.kernel32.GetTickCount() - lii.dwTime) / 1000.0 
 
 def capture_screen():
-    return pyautogui.screenshot()
+     with mss.mss() as sct:
+        monitors = sct.monitors[1:]
+        screenshots = []
+        for i, monitor in enumerate(monitors):
+            screenshot = sct.grab(monitor)
+            img = Image.frombytes("RGB", (screenshot.width, screenshot.height - TASKBAR_HEIGHT), screenshot.rgb)
+            screenshots.append(img)
+        
+        total_width = sum(img.width for img in screenshots)
+        max_height = max(img.height for img in screenshots)
+        combined_image = Image.new("RGB", (total_width, max_height))
+
+        x_offset = 0
+        for img in screenshots:
+            combined_image.paste(img, (x_offset, 0))
+            x_offset += img.width
+
+        return combined_image
 
 def images_are_different(img1, img2):
     diff = ImageChops.difference(img1, img2)
