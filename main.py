b57db67d1589f7ad0ff332d46d9fb84f60e08d94
Dimitar <dimitar.georgiev@primeholding.com>
2025-03-13 16:00:36 +0200

    improve debugging, create auto_start.vbs

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import ctypes
 import mss
 from PIL import Image, ImageChops
 import os
+import logging
+
+DEBUG = False #when set to True, screenshots, containing only the pixels which changed between captures, will be saved and also the activity will be logged in the activity.log file
 
 CHECK_INTERVAL = 2  # The check interval in seconds
-INACTIVITY_THRESHOLD = 60 * 3  # The time in seconds of screen inactivity before turning off the monitor
+INACTIVITY_THRESHOLD = 60 * 3  # The time in seconds of screen inactivity before turning off the monitors
 TASKBAR_HEIGHT = 40  # Approximate taskbar height in px in order not to capture the clock change (adjust if needed)
+
+if DEBUG:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', handlers=[
+        logging.FileHandler("activity.log"),
+        logging.StreamHandler()
+    ])
 
 def turn_off_monitor():
     ctypes.windll.user32.PostMessageW(0xFFFF, 0x0112, 0xF170, 2)
@@ -43,8 +52,17 @@ def capture_screen():
         return combined_image
 
 def images_are_different(img1, img2):
-    diff = ImageChops.difference(img1, img2)
-    return np.any(np.array(diff))
+    diff = ImageChops.difference(img1, img2)    
+    
+    isDiff=np.any(np.array(diff));
+    
+    if DEBUG & isDiff:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        if not os.path.exists("screenshots"):
+            os.makedirs("screenshots")
+        diff.save(f"screenshots/screenshot_{timestamp}.png")
+        
+    return isDiff
 
 last_screenshot = capture_screen()
 last_change_time = time.time()
@@ -55,21 +73,21 @@ while True:
 
     if monitor_is_off:
         if get_idle_time() < 2: 
-            print("User activity detected, resuming monitoring...")
+            logging.info("User activity detected, resuming monitoring...")
             last_change_time = time.time()
             monitor_is_off = False
         continue
 
     current_screenshot = capture_screen()
 
-    dif=images_are_different(last_screenshot, current_screenshot)
+    dif = images_are_different(last_screenshot, current_screenshot)
     
-    print(dif)
+    logging.info(f"Difference detected: {dif}")
     
     if dif:
         last_change_time = time.time()
     elif time.time() - last_change_time > INACTIVITY_THRESHOLD:
-        print("Turning off monitor...")
+        logging.info("Turning off monitor...")
         turn_off_monitor()
         monitor_is_off = True
 
